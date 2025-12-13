010a6f5c5f9fa2c1a1c35515a91386fd2c0f43b5
romain-gossiaux <romain.gossiaux@condorcet.be>
2025-12-13 13:50:02 +0100

    Initial Spring Boot project setup

---
+++ src/main/java/be/condorcet/movie_rental_api/MovieRentalApiApplication.java
@@ -0,0 +1,13 @@
+package be.condorcet.movie_rental_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieRentalApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieRentalApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/condorcet/movie_rental_api/MovieRentalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package be.condorcet.movie_rental_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieRentalApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
